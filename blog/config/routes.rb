68d2a4e9c8ff0224e5a3060435b89e83f266fa82
sti136 <tlo@labci.com>
2022-04-30 14:27:41 +0800

    add articles controller

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  #resources :articles
   # Defines the root path route ("/")
+  #root "articles#index"
   root "articles#index"
+
+  get "/articles", to: "articles#index"
 end
